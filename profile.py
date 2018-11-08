64ac08a449229dec624c9931b5d7e2bce7c2075a
navarka.all@gmail.com <navarka.all@gmail.com>
2018-11-08 14:51:37 +0300

    Add class support

--- profile.py
+++ profile.py
@@ -1,13 +1,43 @@
 from time import time
 from functools import wraps
+from types import FunctionType
 
-def profile(func):
+def profile_class(klass):
+    for attr_name in klass.__dict__:
+        attr = getattr(klass, attr_name)
+        if callable(attr):
+            attr = profile_method(attr)
+            setattr(klass, attr_name, attr)
+    return klass
+
+
+def profile_method(method):
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        print(f"Started {type(self).__name__}.{method.__name__}")
+        t_start = time()
+        result = method(self, *args, **kwargs)
+        t_stop = time()
+        print(f"Finished {type(self).__name__}.{method.__name__}, execution time: {t_stop - t_start}")
+        return result
+    return wrapper
+
+
+def profile_function(func):
     @wraps(func)
-    def wrap(*args, **kwargs):
+    def wrapper(self, *args, **kwargs):
         print(f"Started {func.__name__}")
         t_start = time()
-        result = func(*args, **kwargs)
+        result = func(self, *args, **kwargs)
         t_stop = time()
         print(f"Finished {func.__name__}, execution time: {t_stop - t_start}")
         return result
-    return wrap
+    return wrapper
+
+
+def profile(obj):
+    if isinstance(obj, FunctionType):
+        obj = profile_function(obj)
+    else:
+        obj = profile_class(obj)
+    return obj
--- test.py
+++ test.py
@@ -1,6 +1,22 @@
 from profile import profile
 
 @profile
+class TestClass():
+    def __init__(self):
+        pass
+
+    def test_simple_load_func(self, n):
+        i = 0
+        while i < n:
+            i ** i
+            i += 1
+
+    def test_recursive_func(self, n):
+        if n > 0:
+            self.test_recursive_func(n - 1)
+
+
+@profile
 def test_simple_load_func(n):
     i = 0
     while i < n:
@@ -13,5 +29,9 @@ def test_recursive_func(n):
         test_recursive_func(n-1)
 
 
-test_simple_load_func(10000)
-test_recursive_func(10)
+test_simple_load_func(1000)
+test_recursive_func(5)
+
+t = TestClass()
+t.test_simple_load_func(1000)
+t.test_recursive_func(5)
