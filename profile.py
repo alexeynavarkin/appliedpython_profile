e12d4aeb8ae04c44d41e12ca057dd32417eed444
navarka.all@gmail.com <navarka.all@gmail.com>
2018-11-16 14:46:01 +0300

    Unify function

--- profile.py
+++ profile.py
@@ -7,31 +7,23 @@ def profile_class(klass):
     for attr_name in klass.__dict__:
         attr = getattr(klass, attr_name)
         if callable(attr):
-            attr = profile_method(attr)
+            attr = profile_function(attr, class_name=klass.__name__)
             setattr(klass, attr_name, attr)
     return klass
 
 
-def profile_method(method):
+def profile_function(method, class_name=None):
     @wraps(method)
-    def wrapper(self, *args, **kwargs):
-        print(f"`{type(self).__name__}.{method.__name__}` started")
+    def wrapper(*args,**kwargs):
+        if class_name:
+            print(f"`{class_name}`.", end="")
+        print(f"`{method.__name__}` started")
         t_start = time()
-        result = method(self, *args, **kwargs)
+        result = method(*args, **kwargs)
         t_stop = time()
-        print(f"`{type(self).__name__}.{method.__name__}` finished in {round(t_stop - t_start, 2)}s")
-        return result
-    return wrapper
-
-
-def profile_function(func):
-    @wraps(func)
-    def wrapper(self, *args, **kwargs):
-        print(f"`{func.__name__}` started")
-        t_start = time()
-        result = func(self, *args, **kwargs)
-        t_stop = time()
-        print(f"`{func.__name__}` finished in {round(t_stop - t_start, 2)}s")
+        if class_name:
+            print(f"`{class_name}`.", end="")
+        print(f"`{method.__name__}` finished in {round(t_stop - t_start, 2)}s")
         return result
     return wrapper
 
